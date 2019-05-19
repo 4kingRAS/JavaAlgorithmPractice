b6e9e05dce5c203a844792066a19cbad20787e11
4kingRAS <4king4ever@gmail.com>
2019-05-19 12:46:26 +0800

    update LCS dp method

--- src/com/kkkk/dynamic/LCS.java
+++ src/com/kkkk/dynamic/LCS.java
@@ -12,9 +12,32 @@ import java.util.Scanner;
 public class LCS {
     public String getLCSrecu(String origin) {
 
-
-
         return "";
+    }
+
+    public static int dpLCS(char[] a, char[] b) {
+        //dp method
+        boolean debug = true;
+        int la = a.length;
+        int lb = b.length;
+
+        int[][] dp = new int[la+1][lb+1];  //外围为0
+        for (int i = 1; i < la+1; i++) {
+            for (int j = 1; j < lb+1; j++) {
+                dp[i][j] = dp[i-1][j-1];
+                if (b[j - 1] == a[i - 1]) {
+                    dp[i][j]++;
+                } else {
+                    dp[i][j] = dp[i][j-1] > dp[i-1][j] ? dp[i][j-1] : dp[i-1][j];
+                }
+
+                if (debug) {
+                    System.out.print(dp[i][j]);
+                }
+            }
+            if (debug) { System.out.println(); }
+        }
+        return dp[la][lb];
     }
 
     public static int lcs(char[] a, int at, char[] b, int bt) {
@@ -39,8 +62,12 @@ public class LCS {
         while (sc.hasNextLine()) {
             line = sc.nextLine().trim();
             String[] a = line.split(" ");
-            int length = lcs(a[0].toCharArray(), a[0].length()-1, a[1].toCharArray(), a[1].length()-1);
+            //int length = lcs(a[0].toCharArray(), a[0].length()-1, a[1].toCharArray(), a[1].length()-1);
+
+            int length = dpLCS(a[0].toCharArray(), a[1].toCharArray());
             System.out.println(length);
+
+
         }
     }
 
