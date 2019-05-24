a75af860f47bee252f5ff79fea6529bf8a2f3825
4kingRAS <4king4ever@gmail.com>
2019-05-24 22:52:08 +0800

    init Vector adt.

--- src/com/kkkk/adt/Vector.java
+++ src/com/kkkk/adt/Vector.java
@@ -1,14 +1,43 @@
 package com.kkkk.adt;
 
+
+import java.util.Scanner;
+
 /**
  * @author Yulin.Wang
  * @date 2019-05-20
  * @description vector class implementation
+ * @see         java.util.Vector
  **/
 //TODO:
 
 public class Vector<E> {
 
+    private int capacity;
+    private int count = 0;
+    private Object[] data;
 
-    
+    public Vector(int capacity) {
+        if (capacity < 0)
+            throw new IllegalArgumentException("Illegal Capacity: "+
+                    capacity);
+        this.capacity = capacity;
+        this.data = new Object[capacity];
+    }
+
+    public Vector() { this(10); }
+
+    public int insert(E element) {
+        data[count] = element;
+        return count++;
+    }
+
+    public static void main(String[] args) {
+        Vector<Integer> s = new Vector<>();
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            System.out.println(s.insert(sc.nextInt()));
+        }
+    }
+
 }
