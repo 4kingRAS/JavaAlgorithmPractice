d30248a30e6a7512483834025b88751d50b5954f
4kingRAS <4king4ever@gmail.com>
2019-09-06 23:50:00 +0800

    update leetcode 42

---
+++ src/com/kkkk/LeetCode/P42WaterI.java
@@ -0,0 +1,52 @@
+package com.kkkk.LeetCode;
+
+/**
+ * @author Yulin.Wang
+ * @date 2019-09-06
+ * @description s
+ **/
+
+/**
+ * 接雨水，找到最高点，从两边遍历，做差，求出蓄水量。
+ * tips: 找到最高点，可以保证低点肯定能包起来。
+ */
+
+public class P42WaterI {
+    public static void main(String[] args) {
+        int[] height = {0,1,0,3,1,0,1,4,2,1,2,4};
+
+        int max = 0;
+        int maxpos = 0;
+        for (int i = 0; i < height.length; i++) {
+            if (height[i] > max) {
+                max = height[i];
+                maxpos = i;
+            }
+        }
+
+        int sum = 0;
+        int t = 0;
+        int th = 0;
+        for (int i = 1; i <= maxpos; i++) {
+            int diff = height[i] - height[i - 1];
+            th -= diff;
+            if (th > 0) {
+                sum += th;
+            } else {
+                th = 0;
+            }
+        }
+
+        th = 0;
+        for (int i = height.length - 2; i >= maxpos; i--) {
+            int diff = height[i] - height[i + 1];
+            th -= diff;
+            if (th > 0) {
+                sum += th;
+            } else {
+                th = 0;
+            }
+        }
+        System.out.println(sum);
+    }
+}
---
+++ src/com/kkkk/tc/Mp1.java
@@ -0,0 +1,39 @@
+package com.kkkk.tc;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * @author Yulin.Wang
+ * @date 2019-09-06
+ * @description s
+ **/
+//TODO:
+
+public class Mp1 {
+    /**
+     * https://www.nowcoder.com/question/next?pid=15241156&qid=325931&tid=27180756
+     */
+
+    static int solution(int[] prices, int budget) {
+        Arrays.sort(prices);
+
+        int remain = budget;
+        int sum = 0;
+        if (budget < prices[0]) return -1;
+        for (int i = prices.length - 1; i >= 0; i--) {
+            sum += remain / prices[i];
+            remain = remain % prices[i];
+        }
+        if (remain > 0) return -1;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[] p = {500,200,100};
+        int b = 900;
+        int res = solution(p, b);
+        System.out.println(res);
+
+    }
+}
