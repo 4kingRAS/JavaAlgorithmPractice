da352e15a6201c6d601093f2b17eea11731ea4a8
4kingRAS <4king4ever@gmail.com>
2021-03-10 17:51:13 +0800

    update p410

--- src/com/interview/codegame/LeetCode/P328_OddEvenLinkedList.java
+++ src/com/interview/codegame/LeetCode/P328_OddEvenLinkedList.java
@@ -47,13 +47,7 @@ public class P328_OddEvenLinkedList {
     }
 
     public static void main(String[] args) {
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(3);
-        head.next.next.next = new ListNode(4);
-        head.next.next.next.next = new ListNode(5);
 
-        ListNode.printNodes(head);
     }
 
 }
--- src/com/interview/codegame/LeetCode/P410_SplitArray.java
+++ src/com/interview/codegame/LeetCode/P410_SplitArray.java
@@ -6,14 +6,26 @@ package com.interview.codegame.LeetCode;
  * @description
  * 输入：nums = [7,2,5,10,8], m = 2
  * 输出：18
- * 解释：
+ *                  数组不能分割
  * 一共有四种方法将 nums 分割为 2 个子数组。 其中最好的方式是将其分为 [7,2,5] 和 [10,8] 。
  * 因为此时这两个子数组各自的和的最大值为18，在所有情况中最小。
- *     https://leetcode-cn.com/problems/split-array-largest-sum/
+ * https://leetcode-cn.com/problems/split-array-largest-sum/
+ *
+ * 两种方法：二分 和 动态规划
+ *
  */
 
 public class P410_SplitArray {
+
+    /**
+     * 1. 构造二分数组，上界为所有元素和，下界为最大元素。
+     * 2.
+     *
+     *
+     */
     public static int splitArray(int[] nums, int m) {
+
+
         return 2;
     }
 
